3df60b7d8aa9134a66af02216a1a47e11cd1343f
LFer <lfferreiradiaz@gmail.com>
2019-11-26 14:58:45 -0300

    Added electronic invoice

---
+++ electronic_invoice/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import ResPartner
+from . import AccountInvoice
+from . import EinvoiceConfig
+from . import ResConfigSettings
+from . import ResStore
+from . import AccountInvoiceSignProinfo
+# from . import reconciliation_widget
